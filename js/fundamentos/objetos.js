8ec212f3d9c9a42494f44e088ede338db70a11b8
Marcus-Vinicios <marcusv.fl.oliveira@gmail.com>
2022-09-10 12:06:35 -0300

    modulo_js

---
+++ js/fundamentos/objetos.js
@@ -0,0 +1,20 @@
+ const obj_p = {
+        p_name: 'Marcus',
+        p_class: 'Rogue',
+        powers:{
+                power_01: 'firebolt',
+                power_02: 'moonblast',
+                power_03: 'frostbreath',
+                power_04: 'hidropump', 
+            },
+        stats:{
+                hp: 50,
+                strenght: 100,
+                speed: 75,
+                damage: 8,
+                smartnes: 10,
+                mana: 30,
+                stamina: 45
+            }
+    }
+console.log(obj_p)
--- js/objetos.js
+++
@@ -1,5 +0,0 @@
-const prod1 = {}
-prod1.nome = 'Celular Ultra Mega'
-prod1.preco = "4.998,99"
-
-console.log(prod1)
